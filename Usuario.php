7071396a927ad9a1711885dd16a4957f192934f2
Walther <wszin06@gmail.com>
2024-05-28 19:19:31 -0300

    alteração

--- Usuario.php
+++ Usuario.php
@@ -21,37 +21,45 @@ class Usuario
     {
         return $this->id;
     }
+
     public function getNome()
     {
         return $this->nome;
     }
+
     public function getSenha()
     {
         return $this->senha;
     }
+
     public function getEmail()
     {
         return $this->email;
     }
+
     public function getToken()
     {
         return $this->token;
     }
 
+
     public function setNome($nome)
     {
         $this->nome = $nome;
     }
+
     public function setSenha($senha)
     {
         $this->senha = $senha;
     }
+
     public function setEmail($email)
     {
         $this->email = $email;
     }
+
     public function setToken($token)
     {
         $this->token = $token;
-    }
+    }    
 }
